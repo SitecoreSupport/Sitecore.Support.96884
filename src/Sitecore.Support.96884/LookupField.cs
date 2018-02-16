2b971c49d66d2d1787cfc1bf6d5e17958ae366f8
DK\AEC <aec@sitecore.net>
2018-02-16 09:54:32 +0200

    commit patch

---
+++ src/Sitecore.Support.96884/LookupField.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Support.Data.Fields
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Links;
+  using Sitecore.Data.Fields;
+
+  public class LookupField : Sitecore.Data.Fields.LookupField
+  {
+    public LookupField(Field innerField) : base(innerField)
+    {
+    }
+
+    public override void RemoveLink(ItemLink itemLink)
+    {
+      Assert.ArgumentNotNull(itemLink, "itemLink");
+      if (itemLink.TargetItemID == this.TargetID)
+      {
+        this.Clear();
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.96884/ValueLookupField.cs
@@ -0,0 +1,26 @@
+﻿namespace Sitecore.Support.Data.Fields
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Web;
+  using Sitecore.Configuration;
+  using Sitecore.Data.Items;
+  using Sitecore.Diagnostics;
+  using Sitecore.Links;
+  using Sitecore.Web.UI.HtmlControls.Data;
+  using Sitecore.Data.Fields;
+
+  public class ValueLookupField : Sitecore.Data.Fields.ValueLookupField
+  {
+    public ValueLookupField(Field innerField) : base(innerField)
+    {
+     
+    }
+    public override void RemoveLink(ItemLink itemLink)
+    {
+      Assert.ArgumentNotNull(itemLink, "itemLink");
+      this.Clear();
+    }
+  }
+}
